4a1f5f3c7ba52c3c533e8d3fdf4fc530de338e30
rusiru0629-lgtm <rusiru0629@gmail.com>
2025-08-28 09:41:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "zd4l0QoC#rGjowm5ZZczZRLGRkUnVwiDc3ab-kTVR9f6eR7Ktn60", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
